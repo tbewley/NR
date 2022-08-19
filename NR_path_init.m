fe1aa9900443504d086c156a685e773abfc23216
Thomas Bewley <bewley@eng.ucsd.edu>
2022-08-18 20:20:04 -0700

    path init

--- NR_path_init.m
+++ NR_path_init.m
@@ -6,7 +6,7 @@
 % by appending calls to RR_path_init and NR_path_init in the startup.m file of your userpath directory.
 % Matlab provides guidance on this subject here:  https://www.mathworks.com/help/matlab/ref/startup.html
 %
-% In short, first type the command 'userpath' in Matlab, which will return the name of a directory.
+% In short, fire up Matlab, and type the command 'userpath', which will return the name of a directory.
 % WITHIN THAT DIRECTORY (important!), edit the file startup.m (or, create a file of this name
 % if one doesn't already exist); this file should contain (at least) the following two lines:
 %    RRbase='/Users/bewley/RR'; cd RRbase; RR_path_init
@@ -21,19 +21,15 @@
 % Copyright 2022 by Thomas Bewley, distributed under BSD 3-Clause License.
 
 format compact, close all
-% IMPORTANT: modify the definition of RRbase in the startup.m code (see above) to indicate the
-% full path to the RR codebase on your computer.
+% IMPORTANT: modify the definition of NRbase in the startup.m code (see above) to indicate the
+% full path to the NR codebase on your computer.
 addpath(strcat(NRbase,'/chap01'),strcat(NRbase,'/chap02'),strcat(NRbase,'/chap03'), ...
         strcat(NRbase,'/chap04'),strcat(NRbase,'/chap05'),strcat(NRbase,'/chap06'), ...
         strcat(NRbase,'/chap07'),strcat(NRbase,'/chap08'),strcat(NRbase,'/chap09'), ...
         strcat(NRbase,'/chap10'),strcat(NRbase,'/chap11'),strcat(NRbase,'/chap12'), ...
         strcat(NRbase,'/chap13'),strcat(NRbase,'/chap14'),strcat(NRbase,'/chap15'), ...
         strcat(NRbase,'/chap16'),strcat(NRbase,'/chap17'),strcat(NRbase,'/chap18'),NRbase)
-disp(newline+"Path set for using the Numerical Renaissance codebase.")
-disp("Note: please use GitHub Desktop to keep your local directory at ")
-disp("           "+NRbase)
-disp("in sync with the base branch of the Numerical Renaissance codebase repository at")
-disp("           https://github.com/tbewley/NR")
-disp("See appendix A of the Renaissance Robotics text for further info."+newline)
-
+disp("Path set for using the Numerical Renaissance (NR) codebase.")
+disp("Note: please use GitHub Desktop to keep "+NRbase)
+disp("in sync with the main NR repository at https://github.com/tbewley/RR"+newline)
 % end script NR_path_init
