659a754484efe399e71f5f7372954a51f3705358
Thomas Bewley <bewley@eng.ucsd.edu>
2022-12-06 09:42:55 -0800

    Update NR_generate_key_test.m

--- chap07/NR_generate_key_test.m
+++ chap07/NR_generate_key_test.m
@@ -4,13 +4,17 @@
 % Copyright 2022 by Thomas Bewley, distributed under BSD 3-Clause License. 
 
 clear, N_bits=16, N_data=4   % Setup.  Need N_bits*N_data<=64.
-lower_limit=0                % The integer limits within which the data must lie.
-upper_limit=2^N_bits-1   
+lower_limit=-2^(N_bits-1)+1  % The integer limits within which the data must lie.
+upper_limit=2^(N_bits-1)-1   
 fac=uint64(2^N_bits)
 key=uint64(0);
+offset=int32(fac/2)
 
 % Generate some random int64 test data within these limits.
-data=randi([lower_limit upper_limit],1,N_data);  data=uint64(data)
+signed_data=randi([lower_limit upper_limit],1,N_data,'int32')
+
+% Shift data to the non_negative integers.
+data=uint64(signed_data+offset);
 
 % The following single line generates the key
 for i=1:N_data; key=key+data(i)*fac^(i-1); end, key
@@ -21,4 +25,5 @@ for i=N_data:-1:1,
     key=key-data_check(i)*fac^(i-1);
 end
 
-data_check
+% Shift data back from the positive integers.
+signed_data_check=int64(data_check)-int64(offset)
