26ec434712007e014e27bfb7e6e48bfd4a01a164
Thomas Bewley <bewley@eng.ucsd.edu>
2022-12-12 15:23:05 -0800

    various tweaks

--- chap07/NR_generate_key_test.m
+++ chap07/NR_generate_key_test.m
@@ -3,7 +3,7 @@
 % Numerical Renaissance codebase, Chapter 7, https://github.com/tbewley/NR
 % Copyright 2022 by Thomas Bewley, distributed under BSD 3-Clause License. 
 
-clear, N_bits=16, N_data=4   % Setup.  Need N_bits*N_data<=64.
+clear, N_bits=10, N_data=6   % Setup.  Need N_bits*N_data<=64.
 lower_limit=-2^(N_bits-1)+1  % The (signed) integer limits within which data must lie.
 upper_limit= 2^(N_bits-1)-1   
 fac=uint64(2^N_bits);  key=uint64(0);
@@ -11,7 +11,7 @@ offset32=int32(fac/2); offset64=int64(offset32)
 
 % Generate some random int64 test data within these limits.
 signed_data=randi([lower_limit upper_limit],1,N_data,'int32')
-% Shift data to the non_negative integers.
+% Shift signed_data to the non_negative integers.
 data=uint64(signed_data+offset32);
 
 % The following single line generates the (uint64) key
@@ -22,5 +22,5 @@ for i=N_data:-1:1,
     data_check(i)=idivide(key,fac^(i-1),'floor');
     key=key-data_check(i)*fac^(i-1);
 end
-% Shift data back from the positive integers.
+% Shift data_check back to signed integers.
 signed_data_check=int64(data_check)-offset64
