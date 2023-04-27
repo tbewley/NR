bd63499aea319cc452fcdc611369e3b3cb8e6af8
Thomas Bewley <bewley@eng.ucsd.edu>
2023-04-27 12:19:32 -0700

    tweaks

--- chap02/exercises/PentaTest.m
+++ chap02/exercises/PentaTest.m
@@ -5,8 +5,6 @@
 
 n=7; m=2; a=randn(n); b=randn(n); c=randn(n); d=randn(n); e=randn(n); G=randn(n,m);
 A=diag(a(3:n),-2) + diag(b(2:n),-1) + diag(c(1:n),0) + diag(d(1:n-1),1) + diag(e(1:n-2),2)
-
-
 X=Penta(a,b,c,d,e,G,n)
 % X=Penta(a,b,c,d,e,G,n)
 error=norm(A*X-G)
