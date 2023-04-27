52e757fedd90b2ad6e9fec282265818b96ae0629
Thomas Bewley <bewley@eng.ucsd.edu>
2023-04-27 12:21:24 -0700

    Update NR_Inverse_Fokker_Planck.m

--- chap19/NR_Inverse_Fokker_Planck.m
+++ chap19/NR_Inverse_Fokker_Planck.m
@@ -46,7 +46,7 @@ PHI=LHS\RHS;
 for i=1:N, for j=1:N, c=i+(j-1)*N; phi_num(i,j)=PHI(c); end, end % rearrange PHI as matrix
 figure(6); mesh(x,y,phi_num);  title('numerically determined phi(x,y)'), xlabel('x'), ylabel('y')
 axis([x(1) x(end) y(1) y(end)  min(min(phi_exact)) max(max(phi_num))])
-figure(6); mesh(x,y,phi_num-phi_exact);  title('phi_error(x,y)'), xlabel('x'), ylabel('y')
+figure(7); mesh(x,y,phi_num-phi_exact);  title('phi_error(x,y)'), xlabel('x'), ylabel('y')
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 function [p,gx,gy,h,phi]=gaussian(N,x,y,xvbar,var,alpha)
 P=diag(var)
