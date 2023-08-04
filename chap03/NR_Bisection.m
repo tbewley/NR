d24ea3a3d17aa9db6bb61ecd4c97ae4dc4476028
Thomas Bewley <bewley@eng.ucsd.edu>
2023-08-03 17:44:28 -0700

    tweaks

--- chap03/NR_Bisection.m
+++ chap03/NR_Bisection.m
@@ -3,7 +3,7 @@ function [x,evals]=NR_Bisection(x1,x2,Compute_f,tol,verbose,p)
 % This function refines the bracket of a root with the bisection algorithm.
 % See also FindRootBracket, FalsePosition.  Verify with BisectionTest.
 % Numerical Renaissance codebase, Chapter 2, https://github.com/tbewley/NR
-% Copyright 2021 by Thomas Bewley, distributed under BSD 3-Clause License. 
+% Copyright 2023 by Thomas Bewley, distributed under BSD 3-Clause License. 
 
 f1=Compute_f(x1,verbose,p); f2=Compute_f(x2,verbose,p); evals=2;
 while x2-x1>tol
