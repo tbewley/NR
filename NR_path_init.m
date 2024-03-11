f77d60c9d2eaff651857f76822d8cfa3874a7558
Thomas Bewley <bewley@eng.ucsd.edu>
2024-03-11 12:19:25 -0700

    Create NR_path_init.m

---
+++ NR_path_init.m
@@ -0,0 +1,4 @@
+disp('Please remove reference to the NR codebase from your startup.m file');
+disp('All NR codes have been consolidated into the Renaissance Repository,');
+disp('if you want to use the NR codes, please clone that respository instead.');
+disp('Please pardon the inconvenience of this reorganization...')
